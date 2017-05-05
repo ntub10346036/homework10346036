a3d4ab039f62e27416d22de472c8c769d09b0492
10346036 <10346036@ntub.edu.tw>
2017-05-04 22:52:35 -0400

    finish edit book feature

--- mybook/app/controllers/books_controller.rb
+++ mybook/app/controllers/books_controller.rb
@@ -3,6 +3,22 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def edit
+      @book = Book.find_by(id: params[:id])
+  end
+  
+  def update
+      @book = Book.find_by(id: params[:id])
+
+      if @book.update(book_params)
+        # 成功
+        redirect_to books_path, notice: "資料更新成功!"
+      else
+        # 失敗
+        render :edit
+      end
+   end
+  
   def new
     @book = Book.new
   end
