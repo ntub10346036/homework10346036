7398e8d282c724b6869f23cdf96c6d02c69a08a4
10346036 <10346036@ntub.edu.tw>
2017-05-04 22:34:32 -0400

    finish add book feature

---
+++ mybook/app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def index 
+    @books = Book.all
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+      @book = Book.new(book_params)
+      if @book.save
+         redirect_to books_path, notice: "added!"
+      else
+         render 'new'
+      end
+   end
+  
+  private
+  def book_params
+     params.require(:book).permit(:title, :indroduction, :author, :price)
+  end
+end
